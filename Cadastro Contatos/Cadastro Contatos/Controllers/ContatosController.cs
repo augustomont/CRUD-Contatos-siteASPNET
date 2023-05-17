1f657f8613ced20fec915a8acae6829c3e8d04e1
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-17 17:31:58 -0300

    conectado ao banco de dados

--- Cadastro Contatos/Cadastro Contatos/Controllers/ContatosController.cs
+++ Cadastro Contatos/Cadastro Contatos/Controllers/ContatosController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cadastro_Contatos.Models;
+using Cadastro_Contatos.Models.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cadastro_Contatos.Controllers
 {
     public class ContatosController : Controller
     {
+        readonly private ApplicationDbContext _db;
+        public ContatosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<ContatosModel> contatos = _db.Contatos;
+            return View(contatos);
         }
     }
 }
---
+++ Cadastro Contatos/Cadastro Contatos/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro_Contatos.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ContatosModel> Contatos { get; set; }
+    }
+}
