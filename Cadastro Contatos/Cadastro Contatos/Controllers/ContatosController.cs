d167f8b910e89979088632f01bed44991e0b1fdc
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-17 15:47:58 -0300

    site base

---
+++ Cadastro Contatos/Cadastro Contatos/Controllers/ContatosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro_Contatos.Controllers
+{
+    public class ContatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cadastro Contatos/Cadastro Contatos/Models/ContatosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cadastro_Contatos.Models
+{
+    public class ContatosModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
